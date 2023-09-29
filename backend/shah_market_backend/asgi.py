a9a5df03626a65a19a2ccb0a6eeb0f2f1c36a1fe
Mr. Shah <vikasshah8218@gmail.com>
2023-09-29 15:10:01 +0530

    error:

--- backend/shah_market_backend/asgi.py
+++ backend/shah_market_backend/asgi.py
@@ -1,6 +1,10 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shah_market_backend.settings')
 
+from django.apps import apps
+from django.conf import settings
+
+apps.populate(settings.INSTALLED_APPS)
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -8,10 +12,7 @@ from web_sockets.routing import websocket_urlpatterns
 from channels.security.websocket import AllowedHostsOriginValidator
 
 from web_sockets.authentication import TokenAuthMiddleware
-from django.apps import apps
-from django.conf import settings
 
-apps.populate(settings.INSTALLED_APPS)
 
 # application = get_asgi_application()
 application = ProtocolTypeRouter({
