7b36517714a967a182c4141a61367568ba05cc2f
Mr. Shah <vikasshah8218@gmail.com>
2023-10-19 21:41:04 +0530

    h

--- backend/main/management/commands/update_data.py
+++ backend/main/management/commands/update_data.py
@@ -20,7 +20,7 @@ from web_sockets.consumers import live_data_to_all
 from asgiref.sync import async_to_sync
 
 def hit_link():
-    print(requests.get("https://www.google.com"))
+    print(requests.get("http://www.thevillagegroupe.ca/"))
     print("---------------------test1-------------------------------")
     stock_url  = 'https://www.nseindia.com/api/equity-stockIndices?index=NIFTY%2050'
     headers = {
